0b098612035d82a89807b66baaa05f2d259298f7
Marco Armenta <drmarcoarmenta@gmail.com>
2024-06-27 14:57:54 -0400

    job for matrices

--- job_matrices.sh
+++ job_matrices.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --account=def-assem #account to charge the calculation
-#SBATCH --time=01:00:00 #hour:minutes:seconds
-#SBATCH --cpus-per-task=40 #number of CPU requested
-#SBATCH --mem=30G #memory requested
+#SBATCH --time=02:00:00 #hour:minutes:seconds
+#SBATCH --cpus-per-task=30 #number of CPU requested
+#SBATCH --mem-per-cpu=30G #memory requested
 
 module load StdEnv/2020 scipy-stack/2023a #load the required module
 source ENV/bin/activate #load the virtualenv (absolute or relative path to where the script is submitted)
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-python matrices_on_epoch.py --nb_workers=$SLURM_CPUS_PER_TASK --default_index 0
+python generate_matrix_statistics.py --nb_workers=$SLURM_CPUS_PER_TASK --default_index 0
