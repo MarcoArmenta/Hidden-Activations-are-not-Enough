196290be1cefa5d92c9e6c7e1b22f985659bdcf8
Marco Armenta <drmarcoarmenta@gmail.com>
2024-09-07 19:27:35 -0400

    Update job_training.sh

--- job_training.sh
+++ job_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account=def-assem #account to charge the calculation
+#SBATCH --account=<ACCOUNT_NAME>  #account to charge the calculation
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=00:30:00 #hour:minutes:seconds
@@ -12,11 +12,11 @@ module load StdEnv/2020 scipy-stack/2023a cuda cudnn #load the required module
 mkdir -p $SLURM_TMPDIR/data/MNIST/
 mkdir -p $SLURM_TMPDIR/data/FashionMNIST/
 echo "Copying datasets..."
-cp -r /home/armenta/scratch/MatrixStatistics/data/MNIST/* $SLURM_TMPDIR/data/MNIST/
+cp -r data/MNIST/* $SLURM_TMPDIR/data/MNIST/
 echo "MNIST ready"
-cp -r /home/armenta/scratch/MatrixStatistics/data/FashionMNIST/* $SLURM_TMPDIR/data/FashionMNIST/
+cp -r data/FashionMNIST/* $SLURM_TMPDIR/data/FashionMNIST/
 echo "Fashion ready"
 
 source ENV/bin/activate #load the virtualenv (absolute or relative path to where the script is submitted)
-echo "Environment ready"
-python training.py --default_index $SLURM_ARRAY_TASK_ID --data_path $SLURM_TMPDIR/data/
+
+python training.py --default_index $SLURM_ARRAY_TASK_ID --temp_dir $SLURM_TMPDIR
