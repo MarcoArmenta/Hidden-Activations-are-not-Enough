24cdee1f7ac44b07cbc28f0d2e30c1dc1b5c6882
Marco Armenta <drmarcoarmenta@gmail.com>
2024-09-07 20:24:29 -0400

    Update job_read_results.sh

--- job_read_results.sh
+++ job_read_results.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --account=def-assem #account to charge the calculation
+#SBATCH --account=<ACCOUNT_NAME> #account to charge the calculation
 #SBATCH --time=06:00:00 #hour:minutes:seconds
+#SBATCH --cpus-per-task=1 #number of CPU requested
+#SBATCH --mem-per-cpu=2G #memory requested
 #SBATCH --array=7,11
 
-module load CCEnv arch/avx512 StdEnv/2020
-module load python/3.9
-source env/bin/activate #load the virtualenv (absolute or relative path to where the script is submitted)
-
-echo "All data ready!"
+module load StdEnv/2020 scipy-stack/2023a #load the required module
+source ENV/bin/activate #load the virtualenv (absolute or relative path to where the script is submitted)
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 python read_results.py --default_index $SLURM_ARRAY_TASK_ID
 echo "Grid search finished !!!"
