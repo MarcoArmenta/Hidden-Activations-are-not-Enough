63eb881020b1b24905df3efcbb6c0c9596be7d85
Marco Armenta <drmarcoarmenta@gmail.com>
2024-09-07 20:17:14 -0400

    Update job_generate_adv_ex.sh

--- job_generate_adv_ex.sh
+++ job_generate_adv_ex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account=def-ko1 #account to charge the calculation
+#SBATCH --account=<ACCOUNT_NAME> #account to charge the calculation
 #SBATCH --time=15:00:00 #hour:minutes:seconds
 #SBATCH --cpus-per-task=10 #number of CPU requested
 #SBATCH --mem-per-cpu=3G #memory requested
@@ -12,15 +12,15 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 mkdir -p $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/weights/
 echo "Copying weights..."
-cp /home/armenta/scratch/MatrixStatistics/experiments/$SLURM_ARRAY_TASK_ID/weights/* $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/weights/
+cp experiments/$SLURM_ARRAY_TASK_ID/weights/* $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/weights/
 echo "Weights copied to temp directory..."
 
 mkdir -p $SLURM_TMPDIR/data/MNIST/
 mkdir -p $SLURM_TMPDIR/data/FashionMNIST/
 echo "Copying datasets..."
-cp -r /home/armenta/scratch/MatrixStatistics/data/MNIST/* $SLURM_TMPDIR/data/MNIST/
+cp -r data/MNIST/* $SLURM_TMPDIR/data/MNIST/
 echo "MNIST ready"
-cp -r /home/armenta/scratch/MatrixStatistics/data/FashionMNIST/* $SLURM_TMPDIR/data/FashionMNIST/
+cp -r data/FashionMNIST/* $SLURM_TMPDIR/data/FashionMNIST/
 echo "Fashion ready"
 
-python generate_adversarial_examples.py --default_index $SLURM_ARRAY_TASK_ID --nb_workers $SLURM_CPUS_PER_TASK --weights_path=$SLURM_TMPDIR --data_path $SLURM_TMPDIR/data/
+python generate_adversarial_examples.py --default_index $SLURM_ARRAY_TASK_ID --nb_workers $SLURM_CPUS_PER_TASK --temp_dir=$SLURM_TMPDIR
