e07455311e02256e25f7d74c9fd48935a133f1e9
Marco Armenta <drmarcoarmenta@gmail.com>
2024-09-07 20:19:58 -0400

    Update job_generate_adv_mats.sh

--- job_generate_adv_mats.sh
+++ job_generate_adv_mats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --account=def-ko1 #account to charge the calculation
+#SBATCH --account=<ACCOUNT_NAME> #account to charge the calculation
 #SBATCH --time=4:00:00 #hour:minutes:seconds
 #SBATCH --cpus-per-task=15 #number of CPU requested
 #SBATCH --mem-per-cpu=2G #memory requested
@@ -12,15 +12,15 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 mkdir -p $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/weights/
 echo "Copying weights..."
-cp /home/armenta/scratch/MatrixStatistics/experiments/$SLURM_ARRAY_TASK_ID/weights/* $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/weights/
+cp experiments/$SLURM_ARRAY_TASK_ID/weights/* $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/weights/
 echo "Weights copied to temp directory..."
 
 mkdir -p $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/adversarial_examples/
 echo "Copying adversarial examples..."
-cp -r /home/armenta/scratch/MatrixStatistics/experiments/$SLURM_ARRAY_TASK_ID/adversarial_examples/* $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/adversarial_examples/
+cp -r experiments/$SLURM_ARRAY_TASK_ID/adversarial_examples/* $SLURM_TMPDIR/experiments/$SLURM_ARRAY_TASK_ID/adversarial_examples/
 echo "Adv examples ready."
 
-mkdir -p /home/armenta/scratch/MatrixStatistics/experiments/$SLURM_ARRAY_TASK_ID/adversarial_matrices/
+mkdir -p experiments/$SLURM_ARRAY_TASK_ID/adversarial_matrices/
 
 python generate_adversarial_matrices.py --nb_workers $SLURM_CPUS_PER_TASK --default_index $SLURM_ARRAY_TASK_ID --temp_dir $SLURM_TMPDIR
 
